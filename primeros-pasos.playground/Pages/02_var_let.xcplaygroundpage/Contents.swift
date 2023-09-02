00ef35012126d312836b0e41121b03649750c290
JavFuentes <javier.antonio8128@gmail.com>
2023-09-01 21:20:34 -0400

    segundo commit

---
+++ primeros-pasos.playground/Pages/02_var_let.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+//Constante
+let maximumNumberOfLoginAttempts = 3
+
+//Variable
+var currentLoginAttempt = 0
+
+var x = 0.0, y = 0.0, z	= 0.0
+
+var welcomeMessage : String
+
+welcomeMessage = "Hola, ¿qué tal?"
+
+var red, green, blue : Double
+
+	
